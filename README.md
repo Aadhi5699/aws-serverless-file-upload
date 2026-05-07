caaac41fbac5fb0e92f05f26b450e851d394959e
Aadhi5699 <aadhi5699@gmail.com>
2026-05-07 09:34:53 +0530

    Added SNS topic to send email

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 This project demonstrates a scalable, event-driven serverless architecture on AWS. It allows users to securely upload files to S3 using pre-signed URLs, triggers Lambda functions for processing, and sends notifications upon completion.
 
+User -> API Gateway -> Upload Lambda -> Pre-Signed URL -> S3 Upload Bucket -> S3 Event Notification -> Processor Lambda -> Processed Bucket -> SNS -> Email Notification
+
 ## Tech Stack
 
 * AWS Lambda
@@ -15,12 +17,14 @@ This project demonstrates a scalable, event-driven serverless architecture on AW
 
 ## Features
 
-* Pre-signed URL based secure uploads
-* Event-driven file processing
-* Notification system
-* Fully serverless & cost-efficient
+- Secure pre-signed uploads
+- API Gateway integration
+- Event-driven Lambda processing
+- SNS email notifications
+- CloudWatch alarms
+- Least privilege IAM
 
 ## Architecture Diagram
-
+![Architecture Diagram](architecture-diagram.png)
 
 ## Setup Guide
--- backend/file-processor/index.js
+++ backend/file-processor/index.js
@@ -3,9 +3,20 @@ import {
     CopyObjectCommand
 } from "@aws-sdk/client-s3";
 
+import {
+    SNSClient,
+    PublishCommand
+} from "@aws-sdk/client-sns";
+
 const s3 = new S3Client({
     region: "ap-south-1"
 });
+
+const sns = new SNSClient({
+    region: "ap-south-1"
+});
+
+const TOPIC_ARN = "arn:aws:sns:ap-south-1:050366487790:file-processing-topic";
 
 export const handler = async (event) => {
 
@@ -16,6 +27,7 @@ export const handler = async (event) => {
         const record = event.Records[0];
 
         const sourceBucket = record.s3.bucket.name;
+
         const sourceKey = decodeURIComponent(
             record.s3.object.key.replace(/\+/g, " ")
         );
@@ -33,6 +45,25 @@ export const handler = async (event) => {
         );
 
         console.log("File copied successfully");
+
+        // SNS Notification
+        await sns.send(
+            new PublishCommand({
+                TopicArn: TOPIC_ARN,
+                Subject: "File Processed Successfully",
+                Message: `
+File processed successfully.
+
+Source:
+${sourceBucket}/${sourceKey}
+
+Processed File:
+${destinationBucket}/${destinationKey}
+                `
+            })
+        );
+
+        console.log("SNS notification sent");
 
         return {
             statusCode: 200,
